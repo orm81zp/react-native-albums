afb5f176f6176007dfd743c8f67c7f3163a1b9b8
Roman Onishchenko <orm81zp@gmail.com>
2020-06-07 01:18:41 +0300

    init

---
+++ index.ios.js
@@ -0,0 +1,12 @@
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+
+// Create a component
+const App = () => {
+  return <Header headerText="Albums" />;
+};
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
