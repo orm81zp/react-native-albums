927ba1ebca375e2e9c42079432669fea713f2733
Roman Onishchenko <orm81zp@gmail.com>
2020-06-07 01:45:24 +0300

    AlbumList

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,17 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => {
-  return <Header headerText="Albums" />;
+  return (
+    <View>
+      <Header headerText="Albums" />
+      <AlbumList />
+    </View>
+  );
 };
 
 // Render it to the device
